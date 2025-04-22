e4fe8ef52e45d3a57151b89f4666ad1d9c2eb041
Jamy <opsjames7@gmail.com>
2025-04-22 10:43:03 +0530

    Update Deployed_Model.py

--- Deployed_Model.py
+++ Deployed_Model.py
@@ -4,33 +4,24 @@ import joblib
 
 # Now I am loading my trained model and scaler here for creating the web app:
 
-my_model = joblib.load("land_mines_randomforest_detection.joblib")
+my_model = joblib.load("land_mines_logisticregression.joblib")
+saved_scaler = joblib.load("land_mines_saved_scaler.joblib")
 
-st.title("Land Mine Detection App (Hopefully ;)")
+st.title("Land Mine Detection App (Hopefully ;) ")
 st.markdown("This app predicts whether a land mine is present or not based on various sensory inputs, " \
 "well I mean hopefully it does... It's not like you are gonna come complaining... right?")
-
-soil_type_values = {
-    1: "Dry and Sandy",
-    2: "Dry and Humus",
-    3: "Dry and Limy",
-    4: "Humid and Sandy",
-    5: "Humid and Humus",
-    6: "Humid and Limy"
-}
 
 v = st.number_input("Output voltage of FLC sensor due to magnetic distortion:", 
                     min_value=0.0, max_value=10.6, step=0.1, value=5.0)
 
-h = st.number_input("Enter the height of the sensor from the ground(cm):", min_value=0.0, max_value=20.0, 
-                    step=0.5, value=10.0)
-
-s = st.number_input("Soil type depending on moisture content(1-6):", min_value=1.0, max_value=6.0, step=1.0,
-                    value=3.0)
+h = st.number_input("Enter the height of the sensor from the ground:", min_value=0.0, max_value=20.0, step=0.5, value=10.0)
+s = st.number_input("Soil type depending on moisture content:", min_value=1.0, max_value=6.0, step=1.0, value=3.0)
 
 if st.button("To mine or not to mine?"):
     input_data = pd.DataFrame([[v, h, s]], columns=["V", "H", "S"])
-    prediction = my_model.predict(input_data)[0]
+    input_scaled = saved_scaler.transform(input_data)
+    input_scaled_df = pd.DataFrame(input_scaled, columns=["V", "H", "S"])
+    prediction = my_model.predict(input_scaled_df)[0]
 
     mine_classes = {
         1: "Nuh uh (Class 1)",
